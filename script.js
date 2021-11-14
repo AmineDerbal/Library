ec7d33b3a4fe8af8c293f7077345eddf47e933a2
Kyu931 <aminedoran31@gmail.com>
2021-11-14 12:52:07 +0100

    still in the beginning

---
+++ script.js
@@ -0,0 +1,13 @@
+class Book {
+  constructor(
+    title = "UNKNOWN",
+    author = "UNKNOWN",
+    pages = 0,
+    isRead = false
+  ) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+  }
+}
